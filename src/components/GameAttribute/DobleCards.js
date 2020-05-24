a1a5fcffd40357344ed2d36ae10587329b1befe7
delissgt <delissgt@gmail.com>
2020-05-24 15:09:28 -0500

    Improved code for valite key number and notification

--- src/components/GameAttribute/DobleCards.js
+++ src/components/GameAttribute/DobleCards.js
@@ -1,11 +1,14 @@
 import React, {useEffect, useState} from "react";
-import {Card, Divider, Col, Row, Button, Radio, Affix} from 'antd';
+import {Card, Divider, Col, Row, Button, Radio, notification} from 'antd';
+import {MehOutlined, SmileOutlined, AppstoreAddOutlined} from '@ant-design/icons';
 import {Avatar} from "antd/es";
 
 import Lion from '../../assets/Animals/lion-48.png';
 import Tiger from '../../assets/Animals/tiger-face-48.png';
 import Leopard from '../../assets/Animals/leopard-48.png';
 import Giraffe from '../../assets/Animals/giraffe-48.png';
+
+import {AttributeGame} from "../../utils/HttpRequestGame";
 
 import "./style.css";
 
@@ -16,73 +19,67 @@ const DobleCards = () => {
     const [answer2, setAnswer2] = useState(0);
     const [answer3, setAnswer3] = useState(0);
 
-    const [buttonState1, setButton1] = useState(false);
-    const [buttonState3, setButton3] = useState(false);
-    const [buttonState4, setButton4] = useState(false);
-    const [buttonState6, setButton6] = useState(false);
-    const [buttonState7, setButton7] = useState(false);
-    const [buttonState9, setButton9] = useState(false);
-
     const handleClick = () => {
-        console.log('click , revisar respuestas');
-        console.log('answer1', answer1);
-        console.log('answer2', answer2);
-        console.log('answer3', answer3);
-        let answer = {
-            AttributesUserAnswerGame2 : {
-                answer1: answer1,
-                answer2: answer2,
-                answer3: answer3,
-            }
-        };
-        console.log('ANSWER send to endpoint', answer);
+
+        if (answer1 === 0 || answer2 === 0 || answer3 === 0){
+            notification.open({
+                message: "Contesta TODAS las Preguntas",
+                icon: <MehOutlined  style={{color: '#FA541C'}} />
+            })
+        } else {
+            let game = {
+                game_level: "game1",
+                user_answer: {
+                    A1: answer1,
+                    A2: answer2,
+                    A3: answer3,
+                }
+            };
+            AttributeGame(game);
+            notification.open({
+                message: "Wooo hoooo!!! respuestas, colocar resultado AQUI",
+                icon: <SmileOutlined  style={{color: '#1890FF'}} />
+            })
+
+        }
+
 
     };
 
 
     useEffect( () => {
         window.addEventListener("keyup", validateKey, false)
-    } , [answer1, answer2, answer3]);
+    } , []);
 
     const validateKey = (event) => {
-        // const e = event.keyCode;
-        // const e = event.key;
-        console.log("event", event);
         let e;
         event.key ? e=event.key : e = event.toString();
 
         switch (e) {
             case "1":
                 setAnswer1(1);
-                setButton1(true);
-                setButton3(false);
                 break;
             case "3":
                 setAnswer1(3);
-                setButton1(false);
-                setButton3(true);
                 break;
             case "4":
                 setAnswer2(4);
-                setButton4(true);
-                setButton6(false);
                 break;
             case "6":
                 setAnswer2(6);
-                setButton4(false);
-                setButton6(true);
                 break;
             case "7":
                 setAnswer3(7);
-                setButton7(true);
-                setButton9(false);
                 break;
             case "9":
                 setAnswer3(9);
-                setButton7(false);
-                setButton9(true);
                 break;
             default:
+                notification.open({
+                    message: "Upppss Tecla no valida",
+                    description: "Activa tu teclado numerico o asegurate de teclear un numero valido del ejercicio",
+                    icon: <AppstoreAddOutlined style={{color: '#00e6bb'}} />
+                });
                 console.log('no hacer nada');
                 break;
         }
@@ -93,14 +90,16 @@ const DobleCards = () => {
 
   return(
      <div>
-         <h1 style={{ backgroundColor: "#90f489"}}>
+         <div style={{ background: "linear-gradient(90deg, rgba(0,230,187,0.5) 0%, rgba(144,244,137,1) 65%, rgba(249,248,113,0.5) 100%)"}}>
+         <h1 >
              Elige la tarjeta correcta de cada par de tarjetas.
          </h1>
-         <h2 style={{ backgroundColor: "#90f489"}}>
+         <h2>
              Da click en el boton "Revisar mis respuestas" cuando hayas terminado.
              TIP: El valor del atributo no describe bien a los atributos del animal.
              Ayudate con la tarjeta del Leon.
          </h2>
+         </div>
 
          <Row gutter={16}>
              <Col span={12}>
@@ -117,9 +116,9 @@ const DobleCards = () => {
                  </Card>
              </Col>
              <Col span={12} >
-                 <Affix offsetTop={20}>
-                     <Button type="primary" size={"large"} onClick={() => {handleClick()}} >Revisar Respuestas</Button>
-                 </Affix>
+                     <Button type="primary" size={"large"} onClick={() => {handleClick()}}
+                             className={'button-check-answer'}
+                     >Revisar Respuestas</Button>
              </Col>
          </Row>
 
@@ -138,24 +137,22 @@ const DobleCards = () => {
                          <p>Peso: Falso</p>
                          <Divider style={{ borderWidth: "2px 0 0" }}/>
                          <p>Targeta 1</p>
-                         {/*<Button onClick={()=> handleClick('1')} size={'large'} block={true} style={{ width: "80%" }}>OK</Button>*/}
                      </Card>
                  </Col>
 
                  <Col span={8}>
-                     <Radio.Group onChange={e => setAnswer1(e.target.value) || validateKey(e.target.value)} size={"large"}>
-                         <Radio.Button value={1} checked={buttonState1}>
+                     <Radio.Group
+                         value={answer1}
+                         onChange={e => setAnswer1(e.target.value)} size={"large"}>
+                         <Radio.Button value={1}>
                              Tarjeta 1
                          </Radio.Button>
                          <Divider type={"vertical"} />
                          <Divider type={"vertical"} />
-                         <Radio.Button value={3} checked={buttonState3}>
+                         <Radio.Button value={3}>
                              Tarjeta 3
                          </Radio.Button>
                      </Radio.Group>
-                     {/*<div>*/}
-                     {/*    <img src={Ok} style={{ height: "40px", width: "40px" }}/>*/}
-                     {/*</div>*/}
                  </Col>
 
                  <Col span={8}>
@@ -170,7 +167,6 @@ const DobleCards = () => {
                          <p>Peso: 200.00 kg</p>
                          <Divider style={{ borderWidth: "2px 0 0" }}/>
                          <p>Tarjeta 3</p>
-                         {/*<Button onClick={()=> handleClick('2')} size={'large'} style={{ width: "80%" }}>OK</Button>*/}
                      </Card>
                  </Col>
              </Row>
@@ -195,13 +191,15 @@ const DobleCards = () => {
                  </Col>
 
                  <Col span={8}>
-                     <Radio.Group onChange={e => setAnswer2(e.target.value) || validateKey(e.target.value)} size={"large"}>
-                         <Radio.Button value={4} checked={buttonState4}>
+                     <Radio.Group
+                         value={answer2}
+                         onChange={e => setAnswer2(e.target.value)} size={"large"}>
+                         <Radio.Button value={4}>
                              Izquierda 4
                          </Radio.Button>
                          <Divider type={"vertical"} />
                          <Divider type={"vertical"} />
-                         <Radio.Button value={6} checked={buttonState6}>
+                         <Radio.Button value={6}>
                              Derecha 6
                          </Radio.Button>
                      </Radio.Group>
@@ -239,18 +237,19 @@ const DobleCards = () => {
                          <p>Peso: 190.00 kg</p>
                          <Divider style={{ borderWidth: "2px 0 0" }}/>
                          <p>Tarjeta 7</p>
-                         {/*<Button onClick={()=> handleClick('1')} size={'large'} block={true} style={{ width: "80%" }}>OK</Button>*/}
                      </Card>
                  </Col>
 
                  <Col span={8}>
-                     <Radio.Group onChange={e => setAnswer3(e.target.value) || validateKey(e.target.value)} size={"large"}>
-                         <Radio.Button value={7} checked={buttonState7}>
+                     <Radio.Group
+                         value={answer3}
+                         onChange={e => setAnswer3(e.target.value)} size={"large"}>
+                         <Radio.Button value={7}>
                              Izquierda 7
                          </Radio.Button>
                          <Divider type={"vertical"} />
                          <Divider type={"vertical"} />
-                         <Radio.Button value={9} checked={buttonState9}>
+                         <Radio.Button value={9}>
                              Derecha 9
                          </Radio.Button>
                      </Radio.Group>
@@ -268,7 +267,6 @@ const DobleCards = () => {
                          <p>Peso: 190.0.0 kg</p>
                          <Divider style={{ borderWidth: "2px 0 0" }}/>
                          <p>Tarjeta 9</p>
-                         {/*<Button onClick={()=> handleClick('2')} size={'large'} block={true} style={{ width: "80%" }}>OK</Button>*/}
                      </Card>
                  </Col>
              </Row>
