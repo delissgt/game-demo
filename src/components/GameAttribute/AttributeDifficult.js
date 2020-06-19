3f8abd2bc224f12ea764cd90501761876c286112
delissgt <delissgt@gmail.com>
2020-06-19 14:15:21 -0500

    Testing Artyom for speech recognition IN PROCESS

--- src/components/GameAttribute/AttributeDifficult.js
+++ src/components/GameAttribute/AttributeDifficult.js
@@ -1,98 +1,134 @@
 import React, {Component} from "react";
+import Artyom from "artyom.js";
+// import Artyom from "artyom.js/source/artyom";
+import AttributeExercise from "./AttributeExercise";
+
 import {Radio, Card, Button, notification} from "antd";
+// import Artyom from "artyom.js/source/artyom";
+// import Artyom from "artyom.js";
 
-import AttributeJava1 from '../../assets/attributes/AttributesJava1.png';
-import AttributeJava2 from '../../assets/attributes/AttributesJava2.png';
-import AttributeJava3 from '../../assets/attributes/AttributesJava3.png';
+// import CodeMirror from "react-codemirror";
+// import "codemirror/lib/codemirror.css";
+// import "codemirror/mode/javascript/javascript";
 
-import AttributeJavaScript1 from '../../assets/attributes/AttributesJs1.png';
-import AttributeJavaScript2 from '../../assets/attributes/AttributesJs2.png';
-import AttributeJavaScript3 from '../../assets/attributes/AttributesJs3.png';
+// import AttributeJava1 from '../../assets/attributes/AttributesJava1.png';
+// import AttributeJava2 from '../../assets/attributes/AttributesJava2.png';
+// import AttributeJava3 from '../../assets/attributes/AttributesJava3.png';
+//
+// import AttributeJavaScript1 from '../../assets/attributes/AttributesJs1.png';
+// import AttributeJavaScript2 from '../../assets/attributes/AttributesJs2.png';
+// import AttributeJavaScript3 from '../../assets/attributes/AttributesJs3.png';
+//
+// import AttributePython1 from '../../assets/attributes/AttributesPython1.png';
+// import AttributePython2 from '../../assets/attributes/AttributesPython2.png';
+// import AttributePython3 from '../../assets/attributes/AttributesPython3.png';
 
-import AttributePython1 from '../../assets/attributes/AttributesPython1.png';
-import AttributePython2 from '../../assets/attributes/AttributesPython2.png';
-import AttributePython3 from '../../assets/attributes/AttributesPython3.png';
+// const code = ' \n bye bye\n const hi = "hellowwww"{ \n  const val = 1 }';
+// window.SpeechRecognition = window.SpeechRecognition || window.SpeechRecognition;
+// const recognition = new window.SpeechRecognition();
+// recognition.onresult = (event) => {
+//     const speechToText = event.results[0][0].transcript;
+// };
+// recognition.start();
+
+
+const Jarvis = new Artyom();
+
 
 class AttributeDifficult extends Component {
-    state = {
-        value: 1,
-        qualification: 0,
-        code1: AttributeJava1,
-        code2: AttributeJava2,
-        code3: AttributeJava3,
+    constructor(props, context) {
+        super(props, context);
 
-    };
+        this.startAssistant = this.startAssistant.bind(this);
+        this.stopAssistant = this.stopAssistant.bind(this);
+        this.speakText = this.speakText.bind(this);
+        this.handleTextareaChange = this.handleTextareaChange.bind(this);
 
-    onChange = e => {
-        console.log('radio checked', e.target.value);
-        this.setState({
-            value: e.target.value,
+        this.state = {
+            artyomActive: false,
+            textAreaValue: "",
+            artyomIsReading: false
+        };
 
+        let CommandsManager = new AttributeExercise(Jarvis);
+        CommandsManager.loadCommands();
+    }
+
+    startAssistant(){
+        let _this = this;
+
+        console.log('Artyom inicio BIEN!');
+        Jarvis.initialize({
+            lang: "en-GB",
+            debug: true,
+            continuous: true,
+            soundex: true,
+            listen: true
+        })
+            .then(() => {
+                console.log(Jarvis.getAvailableCommands());
+                Jarvis.say("hello there, how are you ?");
+
+                _this.setState({artyomActive: true});
+            })
+            .catch((err) => {
+                console.error("OOOOoops daisy", err);
+
+                _this.setState({artyomActive: false})
+            });
+    }
+
+    stopAssistant() {
+        let _this = this;
+
+        Jarvis.fatality().then(() => {
+            console.log("Jarvis has been succesfully stopped");
+
+            _this.setState({ artyomActive: false });
+
+        }).catch((err) => {
+            console.error("Oopsy daisy, this shouldn't happen neither!", err);
+
+            _this.setState({ artyomActive: false });
         });
+    }
+
+    speakText() {
+        let _this = this;
+
+        _this.setState({artyomIsReading: true});
+
+        Jarvis.say(_this.state.textAreaValue, {
+            onEnd() {
+                _this.setState({ artyomIsReading: false });
+            }
+        })
     };
 
-    onChangeCode = e => {
-        if (e.target.value === 'a'){
-            this.setState({code1: AttributeJava1, code2: AttributeJava2, code3: AttributeJava3});
-        }else if (e.target.value === 'b'){
-            this.setState({code1: AttributeJavaScript1, code2: AttributeJavaScript2, code3: AttributeJavaScript3});
-        }else {
-            this.setState({code1: AttributePython1, code2: AttributePython2, code3: AttributePython3});
-        }
-    };
-
-    checkAnswer = () => {
-      if (this.state.value === 3){
-          this.setState({qualification: 100});
-          notification.success({
-              message: 'GENIAL',
-              description: 'reto superado!'
-          })
-      }else{
-      notification.error({
-          message: 'ho nooo!, Intentalo de nuevo...',
-          description: 'este codigo tiene un error'
-      })
-      }
-
-    };
+    handleTextareaChange(event) {
+        this.setState({textareaValue: event.target.value});
+    }
 
     render() {
-        // console.log('che', this.state);
         return (
             <div>
-                <div>
-                <h2>Aquí hay solo una clase bien definida, elige la correcta.</h2>
-                 <h4>Nota: Puedes elegir el lenguaje que estas aprendiendo</h4>
-                </div>
-                <div>
-                    <Radio.Group defaultValue="a" buttonStyle="solid" onChange={this.onChangeCode}>
-                        <Radio.Button value="a">Java</Radio.Button>
-                        <Radio.Button value="b">JavaScript</Radio.Button>
-                        <Radio.Button value="c">Python</Radio.Button>
-                    </Radio.Group>
-                </div>
-            <Radio.Group onChange={this.onChange} value={this.state.value}>
-                <Radio value={1} style={{ display: "inline-flex"}} >
-                    <Card style={{ width: "80%" }} cover={<img alt="exa" src={this.state.code1}/>} />
-                </Radio><br/>
-                <Radio value={2} style={{ display: "inline-flex"}}>
-                    <Card style={{ width: "80%" }} cover={<img alt="exa" src={this.state.code2}/>} />
-                </Radio>
-                <Radio value={3} style={{ display: "inline-flex"}}>
-                    <Card style={{ width: "80%" }} cover={<img alt="exa" src={this.state.code3}/>} />
-                </Radio>
-            </Radio.Group>
-                <div style={{ paddingTop: "2%" }} >
-                    <Button type="primary" ghost
-                        onClick={()=> {this.checkAnswer()}}>Revisar Ejercicio
-                    </Button>
-                </div>
+                <h1>Welcome to Jarvis Assistant DELISSS HERE!</h1>
+                <p>In this very basic assistant, you can say hello and ask for some reports e.g `Generate report of April of this year`</p>
+                {/* Voice commands action buttons */}
+                <input type="button" value="Start Artyom" disabled={this.state.artyomActive} onClick={this.startAssistant}/>
+                <input type="button" value="Stop Artyom" disabled={!this.state.artyomActive} onClick={this.stopAssistant}/>
+
+                {/* Speech synthesis Area */}
+
+                <p>I can read some text for you if you want:</p>
+
+                <textarea rows="5" onChange={this.handleTextareaChange} value={this.state.textareaValue}/>
+                <br/>
+                {/* Read the text inside the textarea with artyom */}
+                <input type="button" value="Read Text" disabled={this.state.artyomIsReading} onClick={this.speakText}/>
             </div>
-        );
+        )
     }
 }
 
 export default AttributeDifficult;
-
-// ReactDOM.render(<App />, mountNode);
--- src/components/GameAttribute/AttributeExercise.js
+++ src/components/GameAttribute/AttributeExercise.js
@@ -1,66 +1,51 @@
 import React, {Component} from "react";
-import toyCar from '../../assets/attributes/ToyCarRed.png';
-import carTransformed from '../../assets/attributes/ToyCarRedTransformed.png';
+// import toyCar from '../../assets/attributes/ToyCarRed.png';
+// import carTransformed from '../../assets/attributes/ToyCarRedTransformed.png';
 import {Button, notification, Radio} from "antd";
 import {GlassMagnifier} from "react-image-magnifiers";
 
+// import Artyom from "artyom.js/source/artyom";
+
+
+// ArtyomCommandsManager
+// ArtyomCommands.js
 class AttributeExercise extends Component {
     // constructor
-    state= {
-        value: 0,
-        sizeRadioButton: 'large',
-        answer: 4,
-        qualification: 0
-    };
-
-    onChange = e => {
-        this.setState({value: e.target.value});
-    };
-
-    checkAnswer= ()=> {
-
-        if(this.state.answer === this.state.value) {
-            this.setState({qualification: 100});
-            notification.success({
-                message: 'GENIAL !!!',
-                description: 'reto superado'
-            })
-        }else {
-            notification.error({
-                message: 'ho nooo! Intentalo de nuevo...',
-                description: 'estos atributos no describen bien al objeto'
-            })
-        }
-    };
-
-    render() {
-
-        return(
-            <div>
-                <h2>Selecciona los atributos que describan mejor al objeto. </h2>
-
-                <GlassMagnifier
-                    imageSrc={carTransformed}
-                    largeImageSrc={toyCar}
-                    style={{ width:500, height:300, display: 'inline-block' }}/>
-
-                <div style={{ marginTop: 16 }}>
-                    <Radio.Group onChange={this.onChange} value={this.state.value} size={this.state.sizeRadioButton}>
-                        <Radio value={1}>numero_de_llantas = 4; color = "rojo"; tiene_ventanas = false;</Radio><br/>
-                        <Radio value={2}>numero_de_patas = 4; color = "verde"; tiene_ventanas = false;</Radio><br/>
-                        <Radio value={3}>numero_de_llantas = 0; color = "rojo"; tiene_ventanas = true;</Radio><br/>
-                        <Radio value={4}>numero_de_llantas = 4; color = "rojo"; tiene_ventanas = true;</Radio><br/>
-                    </Radio.Group>
-                </div>
-                <div style={{ paddingTop: "2%" }}>
-                <Button type="primary" ghost
-                    onClick={()=> {this.checkAnswer()}}>Revisar Ejercicio</Button>
-                </div>
-            </div>
-        );
+    constructor(ArtyomInstance) {
+        super(ArtyomInstance);
+        this._artyom = ArtyomInstance;
     }
 
+    loadCommands() {
+        let Artyom = this._artyom;
 
+        return Artyom.addCommands([
+            {
+                indexes: ["Hello", "Hi"],
+                action: () => {
+                    Artyom.say("Hello, how are you?");
+                }
+            },
+            {
+                indexes: [/How are you/, /Regular expressions supported/],
+                smart: true,
+                action: () => {
+                    Artyom.say("I'm fine, thanks for asking !");
+                }
+            },
+            {
+                indexes: ["Generate reports of * of this year"],
+                smart: true,
+                action: (i, month) => {
+                    let year = new Date().getFullYear();
+
+                    Artyom.say(`Generating reports of ${month} ${year} `);
+
+                    Artyom.say("Ready ! What were you expecting? write some code you lazy bear !");
+                }
+            },
+        ]);
+    }
 }
 
 export default AttributeExercise;
