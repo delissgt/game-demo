9c1910c31b23902a371c79bc9a3a96ad3df408c7
delissgt <delissgt@gmail.com>
2020-08-25 15:08:49 -0500

    App Pretty code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, {Component} from 'react';
-import './App.css';
-import {BrowserRouter, Route } from 'react-router-dom';
-import Login from '../src/containers/Login';
-
-import Games from '../src/containers/Games';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Login from "../src/containers/Login";
+import Games from "../src/containers/Games";
 import Attribute from "../src/components/GameAttribute/Attribute";
 import Methods from "./components/GameMethods/Methods";
 import Settings from "../src/containers/Settings";
@@ -12,18 +11,17 @@ class App extends Component {
     render() {
         return (
             <BrowserRouter>
-            <div className="App">
-                {/*<Route  path="/"  exact render={() => <Login/>}/>*/}
-                <Route path="/login" exact component={Login}  />
-                <Route path='/games' exact component={Games}/>
-                <Route exact path="/games/attributes" component={Attribute} />
-                <Route exact path="/games/methods" component={Methods} />
-                <Route exact path="/settings" component={Settings} />
-            </div>
+                <div className="App">
+                    {/*<Route  path="/"  exact render={() => <Login/>}/>*/}
+                    <Route path="/login" exact component={Login} />
+                    <Route path="/games" exact component={Games} />
+                    <Route exact path="/games/attributes" component={Attribute} />
+                    <Route exact path="/games/methods" component={Methods} />
+                    <Route exact path="/settings" component={Settings} />
+                </div>
             </BrowserRouter>
         );
     }
 }
 
 export default App;
-
