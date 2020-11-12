72ffa320d6bf70cf9d3e0c1699c33937185b9501
delissgt <delissgt@gmail.com>
2020-11-12 12:59:56 -0600

    Start Rate Working and Finished

--- src/components/Cards/Card/Card.js
+++ src/components/Cards/Card/Card.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Card as CardAntd, Rate, Col } from "antd";
-import { TrophyFilled } from "@ant-design/icons";
+import { StarFilled } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import classes from "./Card.css";
 
 const card = props => {
     let cardColor = "#1890ff";
 
+    console.log('PROPS EN CARD', props);
+
     return props.games.map(game => {
         {
             game.url === "#" ? (cardColor = "#d9d9d9") : (cardColor = "#fafafa");
         }
+        console.log('game', game.score);
         return (
             <React.Fragment key={game.id}>
                 <Col span={12}>
@@ -20,9 +23,9 @@ const card = props => {
                             className={classes}
                             title={game.title}
                             bordered={false}
+                            id={"cardAttributes"}
                             actions={[
-                                <Rate character={<TrophyFilled />} />,
-                                // <Rate character={<TrophyFilled />} disabled defaultValue={3}/>,
+                                <Rate character={<StarFilled />} disabled defaultValue={game.score} count={3}/>,
                             ]}
                         >
                             <p>{game.description}</p>
