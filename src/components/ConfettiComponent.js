d45d1a8148751bc842c9a0f936cdd50cd298a6d4
delissgt <delissgt@gmail.com>
2020-11-16 19:57:42 -0600

    Update confetti component
    calculate height and width and run prop for draw or undraw component

--- src/components/ConfettiComponent.js
+++ src/components/ConfettiComponent.js
@@ -1,19 +1,20 @@
-import React from "react";
-// import { Modal } from "antd";
+import React  from "react";
+import {useWindowSize} from "react-use";
 
 //TODO confetti
 // import Confetti from "react-confetti/dist/types/Confetti";
 import ReactConfetti from "react-confetti";
 
-const ConfettiComponent = () => {
-    const {widthWindow, heightWindow } = "100%";
+const ConfettiComponent = (props) => {
+    const {width, height } = useWindowSize();
 
     return (
         <ReactConfetti
-            width={widthWindow}
-            height={heightWindow}
+            width={width}
+            height={height}
             recycle={false}
             numberOfPieces={500}
+            run={props.run}
         />
     )
 
