139a0116df6cffec32437add8fee4d7c735a12c5
delissgt <delissgt@gmail.com>
2021-04-25 15:23:53 -0500

    Build for deployment

---
+++ build/precache-manifest.894f62919ff1786ca8957ebb09db7545.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18890c94f12056133aaef0a42de4651",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a85f1514aef4b1901e1",
+    "url": "/static/css/2.bff3c913.chunk.css"
+  },
+  {
+    "revision": "c32478b6ec535e576415",
+    "url": "/static/css/main.9771e43a.chunk.css"
+  },
+  {
+    "revision": "7a85f1514aef4b1901e1",
+    "url": "/static/js/2.c79b9a1e.chunk.js"
+  },
+  {
+    "revision": "f5e4e110a3d4eadbd3deeb5474b2850d",
+    "url": "/static/js/2.c79b9a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c32478b6ec535e576415",
+    "url": "/static/js/main.08524bf7.chunk.js"
+  },
+  {
+    "revision": "b50ff8cd712cef16889f",
+    "url": "/static/js/runtime-main.403a8228.js"
+  },
+  {
+    "revision": "6687d03f97c87d322a1b8630a9eabf77",
+    "url": "/static/media/Story-p1-attribute.6687d03f.pdf"
+  },
+  {
+    "revision": "9fd2929ab04df4c8c42f26abd15c4645",
+    "url": "/static/media/Story-p2-attribute.9fd2929a.pdf"
+  },
+  {
+    "revision": "2d12a25fcc3101db2a22aa453d4c4d0a",
+    "url": "/static/media/Story-p3-attribute.2d12a25f.pdf"
+  },
+  {
+    "revision": "605b16579de85faca1a096aff508a8a8",
+    "url": "/static/media/Story-p4-attribute.605b1657.pdf"
+  },
+  {
+    "revision": "59905e30a3370af4cf484457f90e9257",
+    "url": "/static/media/Story-p5-method.59905e30.pdf"
+  },
+  {
+    "revision": "7450053d6494974601cebb85bbc2d340",
+    "url": "/static/media/Story-p6-method.7450053d.pdf"
+  },
+  {
+    "revision": "9816aa73b5030df3a7c33ac283962d6e",
+    "url": "/static/media/Story-p7-method.9816aa73.pdf"
+  },
+  {
+    "revision": "e0ece64da919e46ff5c0291d81e09481",
+    "url": "/static/media/Story-p8-method.e0ece64d.pdf"
+  }
+]);
